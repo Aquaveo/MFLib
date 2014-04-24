e05e718c1a8ba22448ea16feae5a8369582bf56a
bdolinar <bdolinar@8cd6f04c-145c-4a1c-8e6b-d2320b174db3>
2014-04-24 15:09:34 +0000

    Added copies of dependent mfLib libraries into MODELS/external_libs.
    git-svn-id: https://svn.aquaveo.com/xms/devproj/trunk/files_gms/MODELS/mfLib@29682 8cd6f04c-145c-4a1c-8e6b-d2320b174db3

--- ModflowLib/CxxTest/build_test_runner.bat
+++ ModflowLib/CxxTest/build_test_runner.bat
@@ -1,7 +1,7 @@
-@echo on
+@echo off
 python.exe CxxTest\list_test_headers.py ModflowLib.vcxproj > test_headers.txt
 IF NOT %errorlevel% == 0 GOTO :END
-python.exe ..\..\cxxtest-4.2.1\bin\cxxtestgen --have-std --have-eh --main=run_cxxtests_main --headers=test_headers.txt --output=CxxTest\1.0\new_runner.cpp
+python.exe ..\..\external_libs\cxxtest-4.2.1\bin\cxxtestgen --have-std --have-eh --main=run_cxxtests_main --headers=test_headers.txt --output=CxxTest\1.0\new_runner.cpp
 IF NOT %errorlevel% == 0 GOTO :END
 python.exe CxxTest\update_on_changed.py CxxTest\1.0\new_runner.cpp CxxTest\1.0\runner.cpp
 :END
